0bc102ec828804380b6282b233126eaab7aa387f
Daniel Ochuba <danielochuba78@gmail.com>
2023-06-14 21:28:28 +0100

    fetch and display quote using API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import './css/App.css';
 import Calculator from './components/Calculator';
+import Quotes from './components/quotes';
 
 function App() {
   return (
     <div className="calculator-container">
       <div className="body">
         <Calculator />
+        <Quotes />
       </div>
 
     </div>
