cb592be37fb0bf702fb3f237f14ca3328af22eee
Daniel Ochuba <danielochuba78@gmail.com>
2023-06-20 22:44:08 +0100

    Modify Calculator page layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './css/App.css';
 import Calculator from './components/Calculator';
-import Quotes from './components/quotes';
 
 function App() {
   return (
     <div className="calculator-container">
       <div className="body">
         <Calculator />
-        <Quotes />
       </div>
 
     </div>
